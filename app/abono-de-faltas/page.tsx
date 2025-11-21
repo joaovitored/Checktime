1f3a3e7c1dff0aa740ab2655e6e894f7a72255d2
Father-Kos <iuriallexander17@gmail.com>
2025-11-21 20:01:01 -0300

    Adcionando as paginas de Abono de faltas, e Ajuste de Ponto
    adcionando ultimas paginas

---
+++ app/abono-de-faltas/page.tsx
@@ -0,0 +1,4 @@
+import Faltas from "@/components/Abono-de-Faltas/faltas";
+export default function AbonoFaltaPage() {
+  return <Faltas />;
+}
---
+++ app/ajuste-de-ponto/page.tsx
@@ -0,0 +1,4 @@
+import AjustePonto from "@/components/Ajuste-de-Ponto/ajuste";
+export default function AjustePontoPage() {
+  return <AjustePonto />;
+}
