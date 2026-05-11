53fb895e2f50c2a1902250a6035568260dc7ad3d
Isa Clara <isaclarabispo@gmail.com>
2026-05-11 14:37:21 -0300

    SideBar ajustada
    O visual da side-bar foi ajustado e as páginas principais da sidebar foram limpas, retirando códigos desnecessários e css não usados.

--- frontend/components/Ponto-Espelho/PontoEspelhoCard.tsx
+++ frontend/components/Ponto-Espelho/PontoEspelhoCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Sidebar from "../../components/Sidebar/Sidebar";
+
 import styles from "@/styles/Home.module.css";
 import ponto from "@/components/Ponto-Espelho/PontoEspelho.module.css";
 
@@ -10,36 +11,38 @@ export default function RelatorioPage() {
       <Sidebar />
 
       <main className={styles.main}>
-
         <div style={{ position: "relative", marginBottom: "30px" }}>
-        <h1 className={styles.title}>PONTO ESPELHO</h1>
-        <hr className={ponto.tituloLinha} />
+          <h1 className={styles.title}>PONTO ESPELHO</h1>
 
-        <div className={ponto.mesBotao}>Outubro de 2025</div>
+          <hr className={ponto.tituloLinha} />
+
+          <div className={ponto.mesBotao}>Outubro de 2025</div>
         </div>
-        
+
         <div className={ponto.containerResumo}>
           <div className={ponto.resumoLinha}>
             <span>Dias trabalhados:</span>
             <span>22</span>
           </div>
+
           <div className={ponto.resumoLinha}>
             <span>Horas totais:</span>
             <span>176h</span>
           </div>
+
           <div className={ponto.resumoLinha}>
             <span>Horas extras:</span>
             <span>4h</span>
           </div>
+
           <div className={ponto.resumoLinha}>
             <span>Banco de horas:</span>
             <span>+1h40</span>
           </div>
         </div>
 
-        
         <div className={ponto.tableWrapper}>
-          <table className={ponto.table} style={{ width: "100%" }}>
+          <table className={ponto.table}>
             <thead>
               <tr>
                 <th>Data</th>
@@ -49,16 +52,20 @@ export default function RelatorioPage() {
                 <th>Situação</th>
               </tr>
             </thead>
+
             <tbody>
               {Array.from({ length: 12 }).map((_, i) => (
-               <tr key={i} style={{ borderBottom: "1px solid #ddd" }}>
-                <td className={ponto.tableText}>XX/10</td>
-                <td className={ponto.tableText}>08:00</td>
-                <td className={ponto.tableText}>17:00</td>
-                <td className={ponto.tableText}>08:00</td>
-                <td className={`${ponto.ok} ${ponto.tableText}`}>OK</td>
-              </tr>
+                <tr key={i} style={{ borderBottom: "1px solid #ddd" }}>
+                  <td className={ponto.tableText}>XX/10</td>
 
+                  <td className={ponto.tableText}>08:00</td>
+
+                  <td className={ponto.tableText}>17:00</td>
+
+                  <td className={ponto.tableText}>08:00</td>
+
+                  <td className={`${ponto.ok} ${ponto.tableText}`}>OK</td>
+                </tr>
               ))}
             </tbody>
           </table>
